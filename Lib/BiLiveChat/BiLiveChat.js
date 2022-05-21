e9e91f16eb559ab2ef2a2dc6453658c0bbdc2247
Ms_KuroNeko <Ms_KuroNeko@XL8Z.Net>
2022-05-21 11:42:55 +0800

    固定部分方法纳入BiLiveChat.js库 Signed-off-by: Ms_KuroNeko <Ms_KuroNeko@XL8Z.Net>

--- Lib/BiLiveChat/BiLiveChat.js
+++ Lib/BiLiveChat/BiLiveChat.js
@@ -24,4 +24,29 @@ class BiLiveChat {
     static GoBottom() {
         document.getElementById("hr_BottomMark").scrollIntoView(false);
     }
+
+    /**
+     * 生成i标签粉丝牌
+     * - 如果没有牌子则返回空字符串
+     * - 不满意可以自己重写
+     * @param {String} Name 粉丝牌名字
+     * @param {Int} Level 粉丝牌等级
+     * @returns 标准的i标签粉丝牌
+     */
+    static Make_FansMedal(Name, Level) {
+        return Level > 0 ? "<i class = 'FansMedal' name='" + Name + "' level='" + Level + "''> </i>" : "";
+    }
+
+    /**
+     * 根据URL生成img标签头像
+     * - 不满意可以自己重写
+     * @param {String} URL 
+     * @returns 标准的img标签头像
+     */
+    static Make_Avatar(URL) {
+        // 后缀的@64w_64h是B站图床的控制参数，表示64像素宽高
+        // 加1-3c还可以控制图像质量和压缩比，不加不压画质
+        return "<img class = 'Avatar' src='" + URL + "@64w_64h''/>"
+    }
+
 }
